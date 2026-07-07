09a96f417100925aedc2dac1bb48521360a1a749
victormuthumbi-creator <victor.muthumbi@student.moringaschool.com>
2026-07-07 20:03:19 +0300

    npm test successful

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('datejs');
 
-
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers
 };
